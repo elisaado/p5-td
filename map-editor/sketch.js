c4f2d837dbf00465072846fc215d3037a4f74ef6
Eli Saado <eli5saado@gmail.com>
2021-07-08 00:24:57 +0200

    Make canvas only as big as it should be

--- map-editor/sketch.js
+++ map-editor/sketch.js
@@ -1,7 +1,18 @@
+const widthSelector = document.getElementById("width");
+const heightSelector = document.getElementById("height");
+const tileSizeSelector = document.getElementById("tileSize");
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(
+    tileSizeSelector.value * widthSelector.value,
+    tileSizeSelector.value * heightSelector.value
+  );
 }
 
 function draw() {
   background(220);
+
+  fill(255);
+  strokeWeight(2);
+  stroke(0);
 }
